f6397d72696be70355c7800f002b5dab3c6dc204
Daniel <daniel_sousa.p@outlook.com>
2020-05-10 18:03:18 -0300

    update function automatization

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { Builder, By, Key, Util } = require('selenium-webdriver') 
+const { Builder, By, Key } = require('selenium-webdriver') 
 
 async function automatization() {
   let driver =  await new Builder().forBrowser('chrome').build()
-  await driver.get("http://google.com")
-  await driver.findElement(By.name("q")).sendKeys("React Js", Key.ENTER)
+  
+  for(let i = 0; i < 5; i++) {
+    await driver.get("http://google.com")
+    await driver.findElement(By.name("q")).sendKeys(`React JS ${i}`, Key.ENTER)
+  }
 }
 
 automatization()
